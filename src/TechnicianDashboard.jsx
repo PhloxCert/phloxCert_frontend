80878613686cc87b8b2eb4e632f4ef335b64aca7
lucacl <luca.claus02@gmail.com>
2026-03-27 22:19:14 +0100

    small update

--- src/TechnicianDashboard.jsx
+++ src/TechnicianDashboard.jsx
@@ -192,7 +192,7 @@ const TechnicianDashboard = () => {
         })
       });
 
-      await new Promise(r => setTimeout(r, 3000));
+      await new Promise(r => setTimeout(r, 7000));
       await handleSearch(null, selectedBusiness);
       setForm({ fileName: '', expirationDate: '' });
       setFile(null);
